846216e758cda24c47d65e19410a441c6bc62496
ma-bt <mandeerab20@gamil.com>
2023-04-02 22:14:04 +0545

    signout

--- src/pages/ProfileScreen.tsx
+++ src/pages/ProfileScreen.tsx
@@ -15,9 +15,9 @@ const ProfileScreen = () => {
   const auth = getAuth();
   const user = useSelector(selectUser)
   // signOut(auth).then(() => {
-  //   // Sign-out successful.
+  //  console.log(signOut);
   // }).catch((error) => {
-  //   // An error happened.
+  //   console.log(error)
   // });
 
   return (
@@ -26,20 +26,30 @@ const ProfileScreen = () => {
 
 
       <NavBar />
-      <Flex  justifyContent="center" textAlign="left" >
+      <Flex  justifyContent="center"   >
         <VStack >
-        <Heading as='h2' size='xl' >
-          Edit Profile
+        <Heading fontSize="60px"
+         fontWeight="400"
+          borderBottom="1px solid #282c2d" mb="20px" mt="20px"
+       as='h2' size='xl' pt="8%">
+          Edit Your Profile
         </Heading>
-        <Flex>
+        <Flex  >
           <Box>
-            <Image borderRadius="md" src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='Logo' />
+            <Image h="100px" borderRadius="md" src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='Logo' />
 
           </Box>
-          <Box>
-            <Heading>{user.email}</Heading>
-            <Box>
-              <Button onClick={() => auth.signOut()}> Sign Out</Button>
+          <Box ml="25px" flex="1">
+            <Heading bgColor="grey" p="15px" fontSize="15px" pl="20px" as='h5' size='md'>{user.email}</Heading>
+            <Box mt="20px">
+            <Heading textAlign="left" borderBottom="1px solid #282c2d"  fontSize="15px" pb="20px" as='h5' size='md'>Plans</Heading>
+
+              <Button bgColor="red"
+              fontSize="1rem" border="none"
+              p="10px 20px" mt=" 5%"
+               cursor="pointer" width="100%"
+                color="black"
+               onClick={() => auth.signOut()}> Sign Out</Button>
             </Box>
           </Box>
         </Flex>
